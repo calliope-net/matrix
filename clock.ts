009a3de9108fc365656531c8c59d41a9c520849a
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-03-22 14:54:03 +0100

    Aktualisiere clock.ts

--- clock.ts
+++ clock.ts
@@ -2,27 +2,28 @@
 namespace matrix {
 
     //% group="Analog Uhr" subcategory="Uhr"
-    //% block="Markierung Stunde %hour Mittelpunkt x %x y %y || Pixel %pixel"
+    //% block="Markierung Stunde %hour Mittelpunkt x %x y %y Linie %l0 - %l1 || Pixel %pixel"
     //% pixel.shadow="toggleOnOff" pixel.defl=1
     //% inlineInputMode=inline
-    export function hour_mark(hour: number, x: number, y: number, pixel?: boolean) {
-        //for (let Index = 0; Index <= 11; Index++) {
+    export function hour_mark(hour: number, x: number, y: number, l0: number, l1: number, pixel?: boolean) {
+
+        if (between(hour, 0, 2))
+            hour += 9
+        else if (between(hour, 3, 12))
+            hour -= 3
+        else
+            hour = 9
+
         let value = hour * Math.PI * 30 / 180
+
         line(
-            x + Math.cos(value) * 50, // x0
-            y + Math.sin(value) * 50, // y0
-            x + Math.cos(value) * 60, // x1
-            y + Math.sin(value) * 60,  // y1
+            Math.round(x + Math.cos(value) * l0), // x0
+            Math.round(y + Math.sin(value) * l0), // y0
+            Math.round(x + Math.cos(value) * l1), // x1
+            Math.round(y + Math.sin(value) * l1), // y1
             pixel
         )
-
-        //matrix.writeDisplay()
-        //}
-        //matrix.setPixel(0, 0, true)
     }
 
-    /* function radians(degrees: number) {
-        return degrees * (matrix.pi() / 180)
-    } */
 
 } // clock.ts
