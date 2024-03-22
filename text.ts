a1b5d477480da6aa9659334629a726353bc319e8
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-03-22 20:45:46 +0100

    Aktualisiere text.ts

--- text.ts
+++ text.ts
@@ -15,26 +15,79 @@ namespace matrix {
 
         for (let j = 0; j < pText.length; j++) {
 
-            writeBuffer(x, page, getPixel_8x8(pText.charCodeAt(j)))
+            writeBuffer(x, page, getPixel_5x8(pText.charCodeAt(j)))
 
             //bu.write(offset, getPixel_8x8(pText.charCodeAt(j)))
-            x += 8
+            x += 5
         }
         //this.i2cWriteBuffer(bu)
         //control.waitMicros(50)
     }
 
 
-    function getPixel_8x8(pCharCode: number): Buffer {
-        let string8Byte: string
+    //% imageLiteral=1 shim=images::createImage
+    function createImage(i: string): Image {
+        const im = <Image><any>i;
+        return im
+    }
+
+
+
+    //% group="Text schreiben" subcategory="Text"
+    //% block="Bild aus Zeichencode %charCode"
+    export function writeCharImage(charCode: number): Image {
+
+        let i5x8 = createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+
+        let bu = getPixel_5x8(charCode)
+
+        for (let iy = 0; iy < i5x8.height(); iy++) {
+            for (let ix = 0; ix < i5x8.width(); ix++) {
+                i5x8.setPixel(ix, iy, (bu.getUint8(ix) & 2 ** (iy & 7)) != 0)
+            }
+        }
+
+        return i5x8
+    }
+
+
+    function getPixel_5x8(pCharCode: number): Buffer {
+        let string5Byte: string
         if (between(pCharCode, 0x30, 0x3F)) {
 
             //string8Byte = this.x70.get(15)
             switch (pCharCode & 0xF0) { // 16 string-Elemente je 8 Byte = 128
                 //case 0x20: string8Byte = this.x20.get(pCharCode & 0x0F); break
                 case 0x30:
+                    string5Byte = [
+                        "\x3E\x51\x49\x45\x3E", // "0"
+                        "\x00\x42\x7F\x40\x00", // "1"
+                        "\x62\x51\x49\x49\x46", // "2"
+                        "\x22\x41\x49\x49\x36", // "3"
+                        "\x18\x14\x12\x7F\x10", // "4"
+                        "\x27\x45\x45\x45\x39", // "5"
+                        "\x3C\x4A\x49\x49\x30", // "6"
+                        "\x01\x71\x09\x05\x03", // "7"
+                        "\x36\x49\x49\x49\x36", // "8"
+                        "\x06\x49\x49\x29\x1E", // "9"
+                        /* "\x00\x00\x36\x36\x00\x00\x00\x00", // ":"
+                        "\x00\x00\xAC\x6C\x00\x00\x00\x00", // ";"
+                        "\x00\x08\x14\x22\x41\x00\x00\x00", // "<"
+                        "\x00\x14\x14\x14\x14\x14\x00\x00", // "="
+                        "\x00\x41\x22\x14\x08\x00\x00\x00", // ">"
+                        "\x00\x02\x01\x51\x09\x06\x00\x00" // "?" */
+                    ].get(pCharCode & 0x0F)
 
-                    string8Byte = [
+                    /* string8Byte = [
                         "\x00\x3E\x51\x49\x45\x3E\x00\x00", // "0"
                         "\x00\x00\x42\x7F\x40\x00\x00\x00", // "1"
                         "\x00\x62\x51\x49\x49\x46\x00\x00", // "2"
@@ -51,7 +104,7 @@ namespace matrix {
                         "\x00\x14\x14\x14\x14\x14\x00\x00", // "="
                         "\x00\x41\x22\x14\x08\x00\x00\x00", // ">"
                         "\x00\x02\x01\x51\x09\x06\x00\x00" // "?"
-                    ].get(pCharCode & 0x0F)
+                    ].get(pCharCode & 0x0F) */
 
                     break
                 //case 0x40: string8Byte = this.x40.get(pCharCode & 0x0F); break
@@ -60,7 +113,7 @@ namespace matrix {
                 //case 0x70: string8Byte = this.x70.get(pCharCode & 0x0F); break
                 //default: s8 = this.x70.get(15); break
             }
-            return Buffer.fromUTF8(string8Byte)
+            return Buffer.fromUTF8(string5Byte)
         }
         else {
             let b: Buffer = Buffer.fromHex("FFFFFFFFFFFFFFFF")
