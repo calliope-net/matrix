360b4423edf9f32198f7f4b946668571c44ddb04
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-04-01 18:10:26 +0200

    Aktualisiere main.blocks, clock.ts

--- clock.ts
+++ clock.ts
@@ -2,7 +2,7 @@
 namespace matrix {
 
     //% group="Analog Uhr Zeiger"
-    //% block="12 Stunden %hour Mittelpunkt x %x y %y Linie %l0 - %l1 || Pixel %pixel" weight=2
+    //% block="12 Stunden %hour Mittelpunkt x %x y %y Linie %l0 - %l1 || Pixel %pixel" weight=3
     //% pixel.shadow="toggleOnOff" pixel.defl=1
     //% inlineInputMode=inline
     export function hour_mark(hour: number, x: number, y: number, l0: number, l1: number, pixel?: boolean) {
@@ -16,7 +16,7 @@ namespace matrix {
 
 
     //% group="Analog Uhr Zeiger"
-    //% block="60 Minuten %minute Mittelpunkt x %x y %y Linie %l0 - %l1 || Pixel %pixel" weight=1
+    //% block="60 Minuten %minute Mittelpunkt x %x y %y Linie %l0 - %l1 || Pixel %pixel" weight=2
     //% pixel.shadow="toggleOnOff" pixel.defl=1
     //% inlineInputMode=inline
     export function minute_mark(minute: number, x: number, y: number, l0: number, l1: number, pixel?: boolean) {
@@ -37,6 +37,20 @@ namespace matrix {
             Math.round(y + Math.sin(value) * l1), // y1
             pixel
         )
+    }
+
+    //% group="Analog Uhr Zeiger"
+    //% block="zeichne Uhr Mittelpunkt x %x y %y (Radius 24)" weight=1
+    //% x.defl=30 y.defl=23
+    export function writeClock_radius24(x: number, y: number) {
+        rasterCircle(x, y, 24)//30, 23
+
+        writeImage(imageDrehen(digitImage(3), eZeichenDrehen.rechts), x + 17, y - 2)//47, 21
+        writeImage(digitImage(6), x - 2, y + 17)//28, 40
+        writeImage(imageDrehen(digitImage(9), eZeichenDrehen.links), x - 24, y - 5)//6, 18
+        writeImage(hexImage8x8("427F406251494946"), x - 4, y - 23)// 26, 0
+        //   writeDisplay(0, 15, eI2C.I2C_x3D)
+
     }
 
 } // clock.ts
