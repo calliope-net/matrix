18a6e95c6773589430b7ef7148fc546de64cdb1f
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-04-13 20:06:20 +0200

    Aktualisiere main.blocks, eeprom.ts

--- eeprom.ts
+++ eeprom.ts
@@ -21,6 +21,29 @@ namespace matrix { // eeprom.ts
 
 
 
+    //% group="Text in Matrix zeichnen (Zeichensatz aus EEPROM)" color="#FF7F3F" subcategory="EEPROM"
+    //% block="Text Zeile %row von %col bis %end %text || %align Abstand x %dx y %dy %ut x %fx y %fy %eepromStartadresse %i2c" weight=7
+    //% row.min=0 row.max=15 col.min=0 col.max=24 end.min=0 end.max=24 end.defl=15
+    //% text.shadow="matrix_text"
+    //% dx.min=-25 dx.max=25 dx.defl=8 dy.min=-25 dy.max=25 dy.defl=0
+    //% fx.shadow="matrix_eFaktor" fy.shadow="matrix_eFaktor"
+    //% eepromStartadresse.shadow="matrix_eEEPROM_Startadresse"
+    //% inlineInputMode=inline
+    export function writeEEPROM(row: number, col: number, end: number, text: any, align = eAlign.links, dx = 8, dy = 0, ut = eTransparent.u, fx = 1, fy?: number, eepromStartadresse?: number, i2c = eI2Ceeprom.EEPROM_x50) {
+        basic.showNumber(eepromStartadresse)
+        let len = end - col + 1
+        if (between(row, 0, qMatrix.length - 1) && between(col, 0, 24) && between(len, 0, 25)) {
+            let txt = formatText(text, len, align)
+            for (let j = 0; j < txt.length; j++) {
+                writeImage(get5x8EEPROMImage(txt.charCodeAt(j), eepromStartadresse, i2c), col * 8 + j * dx, row * 8 + j * dy, ut, fx, fy)
+            }
+        }
+    }
+
+
+
+
+
     // ========== group="Bild aus EEPROM in Matrix zeichnen" color="#FF7F3F" subcategory="EEPROM"
 
     //% group="Bild aus EEPROM in Matrix zeichnen (128 Byte pro Zeile)" color="#FF7F3F" subcategory="EEPROM"
@@ -34,7 +57,7 @@ namespace matrix { // eeprom.ts
         fromPage = Math.constrain(fromPage, 0, qMatrix.length - 1) // qMatrix.length ist die Anzahl der Pages 8 oder 16
         toPage = Math.constrain(toPage, fromPage, qMatrix.length - 1)
 
-        for (let page = fromPage; page <= toPage; page++) { 
+        for (let page = fromPage; page <= toPage; page++) {
             fillMatrix(page, page, i2cReadEEPROM(eepromStartadresse + (page - fromPage) * 128, 128, i2c))
         }
     }
@@ -80,11 +103,11 @@ namespace matrix { // eeprom.ts
 
 
     //% group="Bild 5x8 aus Text Zeichen" subcategory="EEPROM"
-    //% block="Bild aus ASCII-Code (EEPROM) %charCode %pEEPROM_Startadresse || %i2c" weight=4
+    //% block="Bild aus ASCII-Code (EEPROM) %charCode %eepromStartadresse || %i2c" weight=4
     //% charCode.min=32 charCode.max=127 charCode.defl=48
     //% eepromStartadresse.shadow="matrix_eEEPROM_Startadresse"
     //% blockSetVariable=bild
-    export function eepromImage(charCode: number, eepromStartadresse: number, i2c = eI2Ceeprom.EEPROM_x50): Image {
+    export function get5x8EEPROMImage(charCode: number, eepromStartadresse: number, i2c = eI2Ceeprom.EEPROM_x50): Image {
 
         let bu = i2cReadEEPROM(eepromStartadresse + charCode * 8, 8, i2c)
 
