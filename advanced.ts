18d9a36dadfc95b81446b4b348a8c2145a5e5441
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-04-14 12:02:53 +0200

    Aktualisiere advanced.ts

--- advanced.ts
+++ advanced.ts
@@ -1,6 +1,7 @@
 
 namespace matrix { // advanced.ts
 
+    // ========== group="Kommentar" advanced=true
 
     //% group="Kommentar" advanced=true
     //% block="// %text"
@@ -14,16 +15,16 @@ namespace matrix { // advanced.ts
     //% block="get Matrix[]" weight=8
     export function getMatrix() { return qMatrix }
 
-
     //% group="internen Speicher lesen" advanced=true
     //% block="get Image[]" weight=7
     export function getImages() { return matrix_Images() }
 
 
+
     // ========== group="Funktionen" advanced=true
 
     //% group="Funktionen" advanced=true
-    //% block="%i0 zwischen %i1 und %i2" weight=7
+    //% block="%i0 zwischen %i1 und %i2" weight=8
     export function between(i0: number, i1: number, i2: number): boolean {
         return (i0 >= i1 && i0 <= i2)
     }
@@ -34,13 +35,6 @@ namespace matrix { // advanced.ts
     export function charCodeAt(text: string, index: number) {
         return text.charCodeAt(index)
     }
-
-    //% group="Funktionen" advanced=true
-    //% block="π" weight=2
-    export function pi() { return Math.PI }
-
-
-
 
     //% group="Funktionen" advanced=true
     //% block="formatiere %pText Länge %len || %align" weight=5
@@ -60,7 +54,6 @@ namespace matrix { // advanced.ts
 
     const c25 = "                         " // 25 Leerzeichen
 
-
     export enum eAlign {
         //% block="linksbündig"
         links,
@@ -68,6 +61,13 @@ namespace matrix { // advanced.ts
         rechts
     }
 
+    //% group="Funktionen" advanced=true
+    //% block="π" weight=2
+    export function pi() { return Math.PI }
+
+
+
+    // ========== group="Test Funktionen" color="#7E84F7" advanced=true
 
     //% group="Test Funktionen" color="#7E84F7" advanced=true
     //% block="zeichne alle Text-Zeichen in Matrix"
@@ -89,30 +89,5 @@ namespace matrix { // advanced.ts
         }
     }
 
-
-
-
-/* 
-
-    //% group="Test Funktionen" advanced=true
-    //% block="zeichne alle ASCII Zeichen in Matrix"
-    export function writeCharset() {
-        //clearMatrix()
-        for (let i = 0; i <= 5; i++) {
-            clearImages() // internes Array Images[]
-            for (let j = 0; j <= 15; j++) {
-                pushImage(get5x8CharImage((2 + i) * 16 + j))
-            }
-            writeImageArray(matrix_Images(), 1, 16 + i * 8, 8)
-        }
-
-        let s = "ÄÖÜäöüß€°" // C4 D6 DC E4 F6 FC DF (20)AC B0
-        let charCode: number
-        for (let j = 0; j < s.length; j++) {
-            charCode = s.charCodeAt(j)
-            writeImage(asciiImage(charCode), 1 + (charCode & 0x0F) * 8, (charCode & 0xF0) >>> 1) // y Bit 7654 * 8 = /2 = 1 Bit nach rechts
-        }
-    }
- */
 
 } // advanced.ts
